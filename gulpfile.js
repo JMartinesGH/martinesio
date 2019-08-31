4974a290f29a0b6695fa59d3a3923ded679f351a
Jamie Martines <dev.jpm@gmail.com>
2019-08-31 12:03:31 -0400

    added sourcemaps to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
  
 sass.compiler = require('node-sass');
 
-
 gulp.task('sass', function () {
     return gulp.src('./scss/styles.scss')
-      .pipe(sass.sync().on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('./assets/css'));
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./assets/css'));
 });
 
 
 gulp.task('sass:watch',function() {
-  gulp.watch('scss/styles.scss', gulp.series('sass'));
+    gulp.watch('scss/styles.scss', gulp.series('sass'));
 });
